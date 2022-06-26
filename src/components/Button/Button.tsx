59c22cbcce62a6dab22e318d399d68b3d80b89f7
Alan <alancurtis5000+accounts@gmail.com>
2022-06-26 14:11:32 -0600

    adding storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-interface ButtonProps {
-    label: string;
+export interface ButtonProps {
+  label: string;
 }
 
-const Button = (props: ButtonProps)=>{
-    return <button>{props.label}</button>
-}
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+};
 
 export default Button;
